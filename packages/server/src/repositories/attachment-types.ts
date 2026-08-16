e4da3411dfc7a33873a9d5f6fa8808c3b45a1266
E2E Test <test@test.com>
2026-08-17 05:16:48 +0800

    init

--- packages/server/src/repositories/attachment-types.ts
+++
@@ -1,9 +0,0 @@
-import type { TaskAttachment } from '../types.js';
-
-export interface AttachmentStore {
-  insert(attachment: TaskAttachment): Promise<void>;
-  getByTaskId(taskId: string): Promise<TaskAttachment[]>;
-  getById(id: string): Promise<TaskAttachment | undefined>;
-  deleteById(id: string): Promise<boolean>;
-  countByTaskId(taskId: string): Promise<number>;
-}
